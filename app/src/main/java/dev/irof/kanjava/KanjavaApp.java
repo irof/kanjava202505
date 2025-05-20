ead9b28c8c3111bef023f557fcb94577e8a32ad6
irof <irof.ocean@gmail.com>
2025-05-20 23:48:26 +0900

    init spring

---
+++ app/src/main/java/dev/irof/kanjava/KanjavaApp.java
@@ -0,0 +1,12 @@
+package dev.irof.kanjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanjavaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KanjavaApp.class, args);
+    }
+}
---
+++ app/src/test/java/dev/irof/kanjava/KanjavaAppTest.java
@@ -0,0 +1,12 @@
+package dev.irof.kanjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanjavaAppTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
